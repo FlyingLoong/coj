fc395442fe319dd11c5789d03b20a7f90c7b02cf
unknown <ywu28@YWU28-MOBL1.ccr.corp.intel.com>
2017-03-19 00:37:19 +0800

    client and server available

--- oj-server/server.js
+++ oj-server/server.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/api/v1', restRouter);
 
+app.use(function(req, res) {
+    res.sendFile("index.html", {root: path.join(__dirname, '../public/')});
+})
+
 app.listen(3000, function(){
     console.log("App is listening port 3000...");
 })
